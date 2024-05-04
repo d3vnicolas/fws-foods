5e03ac4583389a4a0fde960b797fadcc85d19366
Nicolas Soares <nicolas.soares@bis2bis.com.br>
2024-05-04 11:29:02 -0300

    :sparkles: Adds categories carousel

---
+++ app/_components/categoryList.tsx
@@ -0,0 +1,21 @@
+import { db } from "../_lib/prisma";
+import CategoryItem from "./categoryItem";
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+
+const CategoryList = async () => {
+  const categories = await db.category.findMany({});
+
+  return (
+    <Carousel className="pt-5">
+      <CarouselContent className="pb-1">
+        {categories.map((category) => (
+          <CarouselItem className="flex-none" key={category.id}>
+            <CategoryItem category={category} />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+};
+
+export default CategoryList;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import CategoryList from "./_components/categoryList";
 import Header from "./_components/header";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <CategoryList />
     </>
   );
 }
