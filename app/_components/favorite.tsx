b085277e4d2202f9242374a28e7d820eaa5013b4
Nicolas Soares <devnicolas.coroa@gmail.com>
2024-05-13 22:07:35 -0300

    :hammer: Improve components

--- app/_components/favorite.tsx
+++ app/_components/favorite.tsx
@@ -1,9 +1,16 @@
 import { Heart } from "lucide-react";
 
-const Favorite = () => {
+interface FavoriteProps {
+  className?: string;
+  size?: number;
+}
+
+const Favorite = ({ className, size = 14 }: FavoriteProps) => {
   return (
-    <div className="absolute right-[10px] top-[10px] flex aspect-square w-7 items-center justify-center rounded-full bg-card-foreground">
-      <Heart fill="white" stroke="white" size={14} />
+    <div
+      className={`absolute right-[10px] top-[10px] flex aspect-square w-7 cursor-pointer items-center justify-center rounded-full bg-card-foreground ${className}`}
+    >
+      <Heart fill="white" stroke="white" size={size} />
     </div>
   );
 };
